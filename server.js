21258871cbcd7234c9a20da37394e209ac22f2c3
benzeroualaossama61 <benzeroualaossama61@gmail.com>
2026-02-09 14:06:19 +0100

    dernier version en backend

--- server.js
+++ server.js
@@ -39,6 +39,8 @@ app.use(
         "http://localhost:5174",
         "http://localhost:5175",
         "http://localhost:3000",
+        "https://linker-fxy0.onrender.com",
+        "https://*.onrender.com",
         process.env.FRONTEND_URL,
       ].filter(Boolean);
 
